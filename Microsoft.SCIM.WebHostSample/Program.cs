a680cfa574d6149cd4655d2e733b781d4072963d
Elias Alves <elias.cunha@dtidigital.com.br>
2023-09-20 18:18:21 -0300

    Revert to startup file

--- Microsoft.SCIM.WebHostSample/Program.cs
+++ Microsoft.SCIM.WebHostSample/Program.cs
@@ -2,176 +2,23 @@
 // Copyright (c) Microsoft Corporation.  All rights reserved.
 //------------------------------------------------------------
 
-using System;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Routing;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
-using Microsoft.SCIM;
-using Microsoft.SCIM.WebHostSample.Provider;
-using Newtonsoft.Json;
-
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-
-IMonitor monitoringBehavior = new ConsoleMonitor();
-IProvider providerBehavior = new InMemoryProvider();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddAuthentication(ConfigureAuthenticationOptions).AddJwtBearer(config =>
+namespace Microsoft.SCIM.WebHostSample
 {
-    ConfigureJwtBearerOptons(config, builder.Environment, builder.Configuration);
-});
-builder.Services.AddControllers().AddNewtonsoftJson(ConfigureMvcNewtonsoftJsonOptions);
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Hosting;
 
-builder.Services.AddSingleton(typeof(IProvider), providerBehavior);
-builder.Services.AddSingleton(typeof(IMonitor), monitoringBehavior);
-
-ConfigureSwagger(builder.Services);
-
-
-WebApplication app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsProduction())
-{
-    app.UseDeveloperExceptionPage();
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHsts();
-app.UseRouting();
-app.UseHttpsRedirection();
-app.UseAuthentication();
-app.UseAuthorization();
-app.MapControllers();
-
-app.UseEndpoints(
-    (IEndpointRouteBuilder endpoints) =>
+    public class Program
     {
-        endpoints.MapDefaultControllerRoute();
-    });
-
-app.Run();
-
-static void ConfigureSwagger(IServiceCollection services)
-{
-    services.AddSwaggerGen(options =>
-    {
-        options.SwaggerDoc("v1", new OpenApiInfo
+        public static void Main(string[] args)
         {
-            Version = "v1",
-            Title = "SCIM Reference Code",
-            Description = "The reference code provided in this repository will help you get started building a SCIM endpoint. It contains guidance on how to implement basic requirements for CRUD operations on a user and group object (also known as resources in SCIM) and optional features of the standard such as filtering and pagination.",
-            Contact = new OpenApiContact
-            {
-                Name = "SCIM Reference Code on GitHub",
-                Email = string.Empty,
-                Url = new Uri("https://github.com/AzureAD/SCIMReferenceCode/wiki"),
-            },
-            License = new OpenApiLicense
-            {
-                Name = "AzureAD/SCIMReferenceCode is licensed under the MIT License",
-                Url = new Uri("https://github.com/AzureAD/SCIMReferenceCode/blob/master/LICENSE"),
-            }
-        });
-
-        // Set the comments path for the Swagger JSON and UI.
-        var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-        var xmlPath = System.IO.Path.Combine(AppContext.BaseDirectory, xmlFile);
-        options.IncludeXmlComments(xmlPath);
-        options.CustomOperationIds(e => $"{string.Join("", e.ActionDescriptor.RouteValues.Values)}");
-        options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme,
-            new OpenApiSecurityScheme
-            {
-                Type = SecuritySchemeType.Http,
-                In = ParameterLocation.Header,
-                Description = "Token to access this API",
-                Name = "Authorization",
-                Scheme = JwtBearerDefaults.AuthenticationScheme
-            });
-        options.AddSecurityRequirement(new OpenApiSecurityRequirement
-                {
-                    { GetSecurityScheme(), Array.Empty<string>() }
-                });
-    });
-}
-
-static OpenApiSecurityScheme GetSecurityScheme()
-{
-    return new()
-    {
-        Reference = new OpenApiReference
-        {
-            Type = ReferenceType.SecurityScheme,
-            Id = JwtBearerDefaults.AuthenticationScheme
+            Program.CreateHostBuilder(args).Build().Run();
         }
-    };
-}
 
-static void ConfigureAuthenticationOptions(AuthenticationOptions options)
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}
-
-static void ConfigureJwtBearerOptons(JwtBearerOptions options, IWebHostEnvironment environment, ConfigurationManager configuration)
-{
-    if (environment.IsDevelopment())
-    {
-        options.TokenValidationParameters =
-           new TokenValidationParameters
-           {
-               ValidateIssuer = false,
-               ValidateAudience = false,
-               ValidateLifetime = false,
-               ValidateIssuerSigningKey = false,
-               ValidIssuer = configuration["Token:TokenIssuer"],
-               ValidAudience = configuration["Token:TokenAudience"],
-               IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Token:IssuerSigningKey"]))
-           };
-    }
-    else
-    {
-        options.Authority = configuration["Token:TokenIssuer"];
-        options.Audience = configuration["Token:TokenAudience"];
-        options.Events = new JwtBearerEvents
-        {
-            OnTokenValidated = context =>
-            {
-                return Task.CompletedTask;
-            },
-            OnAuthenticationFailed = AuthenticationFailed
-        };
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
-
-static Task AuthenticationFailed(AuthenticationFailedContext arg)
-{
-    // For debugging purposes only!
-    string authenticationExceptionMessage = $"{{AuthenticationFailed: '{arg.Exception.Message}'}}";
-
-    arg.Response.ContentLength = authenticationExceptionMessage.Length;
-    arg.Response.Body.WriteAsync(
-        Encoding.UTF8.GetBytes(authenticationExceptionMessage),
-        0,
-        authenticationExceptionMessage.Length);
-
-    return Task.FromException(arg.Exception);
-}
-
-static void ConfigureMvcNewtonsoftJsonOptions(MvcNewtonsoftJsonOptions options)
-{
-    options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
-}
-
---
+++ Microsoft.SCIM.WebHostSample/Startup.cs
@@ -0,0 +1,188 @@
+//------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//------------------------------------------------------------
+
+namespace Microsoft.SCIM.WebHostSample
+{
+    using System;
+    using System.Reflection;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Authentication;
+    using Microsoft.AspNetCore.Authentication.JwtBearer;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Routing;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.IdentityModel.Tokens;
+    using Microsoft.OpenApi.Models;
+    using Microsoft.SCIM;
+    using Microsoft.SCIM.WebHostSample.Provider;
+    using Newtonsoft.Json;
+
+    public class Startup
+    {
+        private readonly IWebHostEnvironment environment;
+        private readonly IConfiguration configuration;
+
+        public IMonitor MonitoringBehavior { get; set; }
+        public IProvider ProviderBehavior { get; set; }
+
+        public Startup(IWebHostEnvironment env, IConfiguration configuration)
+        {
+            this.environment = env;
+            this.configuration = configuration;
+
+            this.MonitoringBehavior = new ConsoleMonitor();
+            this.ProviderBehavior = new InMemoryProvider();
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            void ConfigureMvcNewtonsoftJsonOptions(MvcNewtonsoftJsonOptions options) => options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+
+            void ConfigureAuthenticationOptions(AuthenticationOptions options)
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }
+
+            void ConfigureJwtBearerOptons( JwtBearerOptions options)
+            {
+                if (this.environment.IsDevelopment())
+                {
+                    options.TokenValidationParameters =
+                       new TokenValidationParameters
+                       {
+                           ValidateIssuer = false,
+                           ValidateAudience = false,
+                           ValidateLifetime = false,
+                           ValidateIssuerSigningKey = false,
+                           ValidIssuer = this.configuration["Token:TokenIssuer"],
+                           ValidAudience = this.configuration["Token:TokenAudience"],
+                           IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(this.configuration["Token:IssuerSigningKey"]))
+                       };
+                }
+                else
+                {
+                    options.Authority = this.configuration["Token:TokenIssuer"];
+                    options.Audience = this.configuration["Token:TokenAudience"];
+                    options.Events = new JwtBearerEvents
+                    {
+                        OnTokenValidated = context =>
+                        {
+                            return Task.CompletedTask;
+                        },
+                        OnAuthenticationFailed = AuthenticationFailed
+                    };
+                }
+
+            }
+
+            services.AddAuthentication(ConfigureAuthenticationOptions).AddJwtBearer(ConfigureJwtBearerOptons);
+            services.AddControllers().AddNewtonsoftJson(ConfigureMvcNewtonsoftJsonOptions);
+
+            services.AddSingleton(typeof(IProvider), this.ProviderBehavior);
+            services.AddSingleton(typeof(IMonitor), this.MonitoringBehavior);
+            services.AddEndpointsApiExplorer();
+            ConfigureSwagger(services);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            if (!this.environment.IsProduction())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHsts();
+            app.UseRouting();
+            app.UseHttpsRedirection();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(
+                (IEndpointRouteBuilder endpoints) =>
+                {
+                    endpoints.MapControllers();
+                });
+        }
+
+        private Task AuthenticationFailed(AuthenticationFailedContext arg)
+        {
+            // For debugging purposes only!
+            string authenticationExceptionMessage = $"{{AuthenticationFailed: '{arg.Exception.Message}'}}";
+
+            arg.Response.ContentLength = authenticationExceptionMessage.Length;
+            arg.Response.Body.WriteAsync(
+                Encoding.UTF8.GetBytes(authenticationExceptionMessage), 
+                0,
+                authenticationExceptionMessage.Length);
+
+            return Task.FromException(arg.Exception);
+        }
+
+        private static void ConfigureSwagger(IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "SCIM Reference Code",
+                    Description = "The reference code provided in this repository will help you get started building a SCIM endpoint. It contains guidance on how to implement basic requirements for CRUD operations on a user and group object (also known as resources in SCIM) and optional features of the standard such as filtering and pagination.",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "SCIM Reference Code on GitHub",
+                        Email = string.Empty,
+                        Url = new Uri("https://github.com/AzureAD/SCIMReferenceCode/wiki"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "AzureAD/SCIMReferenceCode is licensed under the MIT License",
+                        Url = new Uri("https://github.com/AzureAD/SCIMReferenceCode/blob/master/LICENSE"),
+                    }
+                });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = System.IO.Path.Combine(AppContext.BaseDirectory, xmlFile);
+                options.IncludeXmlComments(xmlPath);
+                options.CustomOperationIds(e => $"{string.Join("", e.ActionDescriptor.RouteValues.Values)}");
+                options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme,
+                    new OpenApiSecurityScheme
+                    {
+                        Type = SecuritySchemeType.Http,
+                        In = ParameterLocation.Header,
+                        Description = "Token to access this API",
+                        Name = "Authorization",
+                        Scheme = JwtBearerDefaults.AuthenticationScheme
+                    });
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    { GetSecurityScheme(), Array.Empty<string>() }
+                });
+            });
+        }
+
+        private static OpenApiSecurityScheme GetSecurityScheme()
+        {
+            return new()
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = JwtBearerDefaults.AuthenticationScheme
+                }
+            };
+        }
+    }
+}
